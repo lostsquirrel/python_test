77defdc613a7808f018d5ea1a8ff1e1d6b3a63c7
lostsquirrel <songli1988@gmail.com>
2015-03-18 10:37:06 +0800

    update source for print unicode

--- others/work_helper/print_unicode.py
+++ others/work_helper/print_unicode.py
@@ -7,5 +7,5 @@ def unicode2chines(source):
     print source.decode('unicode-escape')
     
 if __name__ == '__main__':
-    content = '\u624b\u673a\u53f7\u4e0d\u5b58\u5728'
+    content = open('unicode_content.txt', 'r').read()
     unicode2chines(content)
