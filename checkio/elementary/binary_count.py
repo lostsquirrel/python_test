d59c9bcff881ce37e34e61a1e03e01aeac2d68f9
lisong <songli1988@gmail.com>
2015-07-14 16:45:53 +0800

    formart code

--- checkio/elementary/binary_count.py
+++ checkio/elementary/binary_count.py
@@ -1,16 +1,16 @@
 # -*- coding:utf-8 -*-
-'''
+"""
 Created on 2015-03-17
 
 @author: lisong
-For the Robots the decimal format is inconvenient. 
-If they need to count to "1", 
-their computer brains want to count it in the binary representation of that number. 
+For the Robots the decimal format is inconvenient.
+If they need to count to "1",
+their computer brains want to count it in the binary representation of that number.
 You can read more about binary here.
 
-You are given a number (a positive integer). 
-You should convert it to the binary format 
-and count how many unities (1) are in the number spelling. 
+You are given a number (a positive integer).
+You should convert it to the binary format
+and count how many unities (1) are in the number spelling.
 For example: 5 = 0b101 contains two unities, so the answer is 2.
 
 Input: A number as a positive integer.
@@ -23,17 +23,17 @@ checkio(4) == 1
 checkio(15) == 4
 checkio(1) == 1
 checkio(1022) == 9
-    
 
-How it is used: How to convert a number to the binary form. 
+
+How it is used: How to convert a number to the binary form.
 It can be useful for Computer Science purposes.
 
 Precondition: 0 < number ≤ 232
 
-Guido van Rossum, the author of Python, 
-is one of our most famous player. 
+Guido van Rossum, the author of Python,
+is one of our most famous player.
 He is writing some really wonderful code reviews for our player solutions.
-'''
+"""
 def checkio(number):
     count = 0
     
@@ -43,7 +43,7 @@ def checkio(number):
             
     return count
 
-#These "asserts" using only for self-checking and not necessary for auto-testing
+# These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
     print type(bin(123))
     assert checkio(4) == 1
