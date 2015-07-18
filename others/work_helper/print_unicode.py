62137a48899ccd3fb59cd95a1768ce802ef51353
lostsquirrel <songli1988@gmail.com>
2015-07-18 13:27:48 +0800

    unicode escape

--- others/work_helper/print_unicode.py
+++ others/work_helper/print_unicode.py
@@ -7,5 +7,7 @@ def unicode2chines(source):
     print source.decode('unicode-escape')
     
 if __name__ == '__main__':
-    content = open('unicode_content.txt', 'r').read()
+    content = '''
+    \u8ba2\u5355\u4e0d\u5b58\u5728(trade_no:1431525873385488)
+    '''
     unicode2chines(content)
