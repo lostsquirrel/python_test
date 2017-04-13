fbd9d66da02754b2ce9c9423fcefdc2481cf4a1a
lostsquirrel <songli1988@gmail.com>
2017-04-13 15:10:58 +0800

    add some unfinished workers

--- others/log_processor/tomcat_mongo_query.py
+++ others/log_processor/tomcat_mongo_query.py
@@ -3,10 +3,13 @@
 def read_log():
     fh = open('catalina.out')
     content = []
+    counter = 0
     for l in fh:
         # print l
         if '[com.luyouchina.train.core.common.util.ModuleUtils#callService:66]' in l:
             content.append(process_line(l))
+            counter += 1
+    print counter
 
     write_to_sql('\n'.join(content))
 
@@ -18,11 +21,13 @@ def process_line(line):
 
     d = line.index('参数')
     x = len('参数')
+    log_time = line[:19]
     module_method = line[a+x:b]
     spend = line[c+x:d].replace('ms，', '')
     param = line[d+x:-2]
-    mysql_format = 'insert into mongo_query(module_method, spend, param) values (\'%s\', %s, \'%s\');'
-    return mysql_format % (module_method.strip(), spend.strip(), param.strip()[:-1])
+    mysql_format = 'insert into mongo_query(module_method, spend, param, log_time) values (\'%s\', %s, \'%s\', \'%s\');'
+    print log_time
+    return mysql_format % (module_method.strip(), spend.strip(), param.strip()[:-1], log_time)
 
 
 def write_to_sql(content):
