5d6b117cc3c20df0f66fb0edd5881f0c8d9a2c09
lostsquirrel <songli1988@gmail.com>
2018-03-14 18:23:24 +0800

    refactor to oop and keep namespace

--- others/docker_image_transfer.py
+++ others/docker_image_transfer.py
@@ -10,51 +10,91 @@ log = logging.getLogger(__name__)
 
 
 aliyun_registry_url = 'registry.cn-hangzhou.aliyuncs.com'
-aliyun_registry_namespace = '/lisong'
 local_registry_url = 'registry.lisong.pub:5000'
-local_registry_namespace = ''
 
 
-def transfer_single(action, image):
-    transfer_actions = {
-        0: transfer2aliyun,
-        1: transfer2origin,
-        2: transfer2local
-    }
-    transfer_actions[action](image)
+class DockerImageTransfer:
+
+    def __init__(self, image, aliyun_registry_namespace, direct):
+        self.tagged_image_name = None
+        self.registry_url = None
+        self.registry_namespace = ''
+        self.aliyun_registry_namespace = aliyun_registry_namespace
+        self.image = image
+        self.direct = False
+        self.parse_image(image)
+
+    def transfer2aliyun(self):
+        """
+        在没有墙的环境下，下载镜像并打为 aliyun 的镜像，再推送
+        """
+        aliyun_image = self.get_aliyun_image()
+        pull_image(self.image)
+        tag_image(self.image, aliyun_image)
+        push_image(aliyun_image)
+
+    def transfer2origin(self):
+        """
+        在工作环境，从 aliyun 拉取对应的镜像，再打为原来的镜像名称
+        """
+        aliyun_image = self.get_aliyun_image()
+        tag_image(aliyun_image, self.image)
+
+    def transfer2local(self):
+        remote_image = self.image
+        if not self.direct:
+            remote_image = self.get_aliyun_image()
+
+        pull_image(remote_image)
+        local_image = self.get_local_image()
+
+        tag_image(remote_image, local_image)
+        push_image(local_image)
+
+    def get_aliyun_image(self):
+        return '%s%s/%s' % (aliyun_registry_url, self.aliyun_registry_namespace, self.tagged_image_name)
+
+    def get_local_image(self):
+        return '%s%s/%s' % (local_registry_url, self.registry_namespace, self.tagged_image_name)
+
+    def parse_image(self):
+        c = self.image.count('/')
+
+        if c > 1:
+            name_space_start = self.image.find("/")
+            name_space_end = self.image.rfind("/")
+            self.tagged_image_name = self.image.strip('\n')[name_space_end + 1:]
+            self.registry_namespace = self.image[name_space_start + 1:name_space_end]
+            self.registry_url = self.image[:name_space_start]
+
+        elif c > 0:
+            sep = self.image.rfind("/")
+            self.tagged_image_name = self.image.strip('\n')[sep + 1:]
+            self.registry_url = self.image[:sep]
+
+        else:
+            self.tagged_image_name = self.image
+
+    def transfer_single(self, act):
+        transfer_actions = {
+            0: self.transfer2aliyun,
+            1: self.transfer2origin,
+            2: self.transfer2local
+        }
+        transfer_actions[act]()
 
 
-def transfer_multiply(action):
+def transfer_multiply(action, registry_namespace, isDirect):
     fh = open('images.txt')
     action %= 10
     log.info("action %s ", action)
     x = 0
+
     for image in fh:
-        transfer_single(action, image.strip('\n'))
+        w = DockerImageTransfer(image.strip('\n'), registry_namespace)
+        w.transfer_single(action, isDirect)
         x += 1
     log.info("%d images transfer" % x)
-
-
-def transfer2origin(image):
-    aliyun_image = pull_from_aliyun(image)
-    tag_image(aliyun_image, image)
-
-
-def transfer2local(image):
-    remote_image = image
-    if image.startswith(aliyun_registry_url):
-        remote_image = pull_from_aliyun(image)
-    else:
-        pull_image(image)
-    local_image = parse_image_name_to_local(image)
-    tag_image(remote_image, local_image)
-    push_image(local_image)
-
-
-def pull_from_aliyun(image):
-    aliyun_image = parse_image_name_to_aliyun(image)
-    pull_image(aliyun_image)
-    return aliyun_image
 
 
 def pull_image(image):
@@ -72,28 +112,6 @@ def tag_image(source_image, target_image):
     os.system('docker tag %s %s' % (source_image, target_image))
 
 
-def transfer2aliyun(image):
-    aliyun_image = parse_image_name_to_aliyun(image)
-    pull_image(image)
-    tag_image(image, aliyun_image)
-    push_image(aliyun_image)
-
-
-def parse_image_name_to_aliyun(image):
-    return transfer_image_name(image, aliyun_registry_url, aliyun_registry_namespace)
-
-
-def parse_image_name_to_local(image):
-    return transfer_image_name(image, local_registry_url, local_registry_namespace)
-
-
-def transfer_image_name(source_image, target_registry_url, target_registry_namespace):
-    tagged_image_name = source_image
-    if source_image.rfind("/") > 0:
-        tagged_image_name = source_image.strip('\n')[source_image.rfind("/") + 1:]
-    return '%s%s/%s' % (target_registry_url, target_registry_namespace, tagged_image_name)
-
-
 if __name__ == '__main__':
     args = sys.argv
     # 0 single image pass by arg tag and push to aliyun
@@ -103,9 +121,19 @@ if __name__ == '__main__':
     # 11 multiply images pass by images.txt  pull from aliyun and tag to origin
     # 12 multiply images pass by images.txt  pull from aliyun and tag to local
     action = int(args[1])
+    registry_namespace = "/lisong"
+    isDirect = False
     if action < 10:
-        transfer_single(action, args[2])
+        if len(args) == 3:
+            registry_namespace = args[3]
+        if len(args) == 4:
+            isDirect = True
+        DockerImageTransfer(args[2].strip('\n'), registry_namespace, isDirect).transfer_single(action)
     else:
-        transfer_multiply(action)
+        if len(args) == 2:
+            registry_namespace = args[2]
+        if len(args) == 3:
+            isDirect = True
+        transfer_multiply(action, registry_namespace, isDirect)
 
 
