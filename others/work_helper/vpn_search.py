31148d95344a466b8f3007d595fff3679372d073
lostsquirrel <songli1988@gmail.com>
2018-03-23 14:39:43 +0800

    add window ping tool

--- others/work_helper/vpn_search.py
+++ others/work_helper/vpn_search.py
@@ -1,64 +1,74 @@
 # -*- coding: utf-8 -*-
-#选择最佳vpn服务器
+# 选择最佳vpn服务器 仅 windows 中文件适用
 
-from subprocess import call
-from subprocess import check_output
+from multiprocessing import Process
 import subprocess
-from db_conn import getIps
-from db_conn import pingInsert
-from db_conn import getPingbyIp
-import time
-import operator
+import re
 
-ipList = getIps()
+ipList = [
+# 'hnd-jp-ping.vultr.com',
+# 'sgp-ping.vultr.com',
+# 'syd-au-ping.vultr.com',
+# 'fra-de-ping.vultr.com',
+# 'ams-nl-ping.vultr.com',
+# 'lon-gb-ping.vultr.com',
+# 'par-fr-ping.vultr.com',
+# 'wa-us-ping.vultr.com',
+# 'sjo-ca-us-ping.vultr.com',
+# 'lax-ca-us-ping.vultr.com',
+# 'il-us-ping.vultr.com',
+# 'tx-us-ping.vultr.com',
+# 'nj-us-ping.vultr.com',
+# 'ga-us-ping.vultr.com',
+# 'fl-us-ping.vultr.com'
+    '140.82.17.199'
+]
 # print check_output("ping", "127.0.0.1")
 # call(["ls", "-l"])
+result = []
 
 
 # msg = '64 bytes from 220.181.111.86: icmp_seq=1 ttl=54 time=39.3 ms'
-def parseMsg(msg):
-    try :
+def parseMsg(contents):
+    try:
         data = dict()
-        # print msg.index('from')
-        # print msg.index(':')
-        data['ip']=msg[msg.index('from') + 5 : msg.index(':',msg.index('from'))]
-        # print msg.index('ttl')
-        data['ttl']=msg[msg.index('ttl') + 4 : msg.index(' ',msg.index('ttl'))]
-        # print msg.index('time')
-        data['time']=msg[msg.index('time') + 5 : msg.index(' ',msg.index('time'))]
-        # print msg.index('ms')
+        lose = contents[-3]
+        data['lost'] = int(re.split(r"\(|%", lose)[1])
+        spent = contents[-1]
+        ss = re.split(r"=|ms", spent)
+        data['delay']=[int(ss[1]), int(ss[3]), int(ss[5])]
+        # print_gbk(spent)
         return data
-    except :
-        print "Parser message %s Error" % msg
-        
-def doPing(ip):
-    p = subprocess.Popen('ping -w 3 %s' % ip, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    for msg in p.stdout.readlines():
-        print msg
-        if (msg.endswith(' ms\n')):
-            data = parseMsg(msg)
-            if not data == None:
-                data = (data['ip'], data['ttl'], data['time'], time.time())
-                pingInsert(data)
-                
-def getAverate(data): 
-    count = 0
-    sum = 0
-    for i in data:
-        count += 1
-        sum += i[0]
-    if (count == 0):
-        return -1
-    
-    return sum / count
-                
+    except:
+        print "Parser message %s Error" % contents
+
+
+def doPing(ip, result):
+    p = subprocess.Popen('ping -n 50 -w 3 %s' % ip, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    data = parseMsg(p.stdout.readlines())
+    if data is not None:
+        data['ip'] = ip
+        print data
+        result.append(data)
+
+
+def print_gbk(msg):
+    print msg.decode('gbk')
+
+
 if __name__ == "__main__":
-    x = dict()
+    plist = list()
     for ip in ipList:
-        ip = ip[0]
-        data = getPingbyIp(ip)
+        # doPing(ip)
+        p = Process(target=doPing, args=(ip,result))
+        p.start()
+        plist.append(p)
+    for p in plist:
+        p.join()
+    sorted(result, cmp=lambda x, y: x.lost > y.lost)
+    for item in result:
+        print item
+    #     ip = ip[0]
+    # data = getPingbyIp(ip)
 #         print data
-        x[ip] = getAverate(data)
-        
-sorted_x = sorted(x.iteritems(), key=operator.itemgetter(1))
-print sorted_x
+#         x[ip] = getAverate(data)
