2b12335f83adf3fb0eb30e2f49bce6da86e80479
lostsquirrel <songli1988@gmail.com>
2018-05-13 22:51:06 +0800

    add decision tree demo

--- mit-600/lec13.py
+++ mit-600/lec13.py
@@ -16,7 +16,7 @@ def fib(n):
 
 
 def mem_fib(n):
-    memo = { 0:1, 1: 1}
+    memo = {0: 1, 1: 1}
     return mem_fib_x(n, memo)
 
 
@@ -27,3 +27,59 @@ def mem_fib_x(n, memo):
     if not memo.has_key(n):
         memo[n] = mem_fib_x(n - 1, memo) + mem_fib_x(n - 2, memo)
     return memo[n]
+
+
+def max_value(w, v, i, aW):
+    """
+       knapsack problem
+    :param w: weight of item
+    :param v: value of item
+    :param i: index of item
+    :param aW: available weight
+    :return:
+    """
+    # print 'maxVal called with:', i, aW
+    global num_calls
+    num_calls += 1
+    print(num_calls)
+
+    if i == 0:
+        if w[i] <= aW:
+            return v[i]
+        else:
+            return 0
+
+    without_i = max_value(w, v, i - 1, aW)
+    # check available weight is not enough for the next item
+    if w[i] > aW:
+        return without_i
+    else:
+        with_i = v[i] + max_value(w, v, i - 1, aW - w[i])
+    return max(with_i, without_i)
+
+def max_value_mem(w, v, i, aW, m):
+    global num_calls
+    num_calls += 1
+    print(num_calls)
+    key = (i, aW)
+    if not m.has_key(key):
+        if i == 0:
+            if w[i] <= aW:
+                return v[i]
+            else:
+                return 0
+
+        without_i = max_value_mem(w, v, i - 1, aW, m)
+        # check available weight is not enough for the next item
+        if w[i] > aW:
+            m[key] = without_i
+            return m[key]
+        else:
+            with_i = v[i] + max_value_mem(w, v, i - 1, aW - w[i], m)
+        m[key] = max(with_i, without_i)
+    return m[key]
+
+
+def max_value_x(w, v, i, aW):
+    m = dict()
+    return max_value_mem(w, v, i, aW, m)
--- mit-600/test/test_lec13.py
+++ mit-600/test/test_lec13.py
@@ -12,6 +12,7 @@ fib_dict = dict()
 # fib_dict[5] = 8
 fib_dict[6] = 13
 
+
 class TestLec13(unittest.TestCase):
 
     def test_fib(self):
@@ -20,9 +21,29 @@ class TestLec13(unittest.TestCase):
     def test_mem_fib(self):
         self.fibx(fib_dict, mem_fib)
 
-    def fibx(self, fib_dict, fiber):
+    def fib_x(self, fib_dict, fiber):
         for item in fib_dict.items():
             print(item)
             first = fiber(item[0])
             self.assertEqual(first, item[1])
 
+    def test_max_value(self):
+        weights = [1, 5, 3, 4]
+        values = [15, 10, 9, 5]
+        res = max_value(weights, values, len(values) - 1, 8)
+        print('max value is %d' % res)
+        self.assertEqual(res, 29)
+
+    def test_max_value_2(self):
+        weights = [1, 1, 5, 5, 3, 3, 4, 4]
+        values = [15, 15, 10, 10, 9, 9, 5, 5]
+        res = max_value(weights, values, len(values) - 1, 8)
+        print('max value is %d' % res)
+        self.assertEqual(res, 48)
+
+    def test_max_value_x(self):
+        weights = [1, 1, 5, 5, 3, 3, 4, 4]
+        values = [15, 15, 10, 10, 9, 9, 5, 5]
+        res = max_value_x(weights, values, len(values) - 1, 8)
+        print('max value is %d' % res)
+        self.assertEqual(res, 48)
