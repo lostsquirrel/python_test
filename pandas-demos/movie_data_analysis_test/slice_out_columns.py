e2d26ad42f30430f008d053ee048b65dbc04cd14
lostsquirrel <songli1988@gmail.com>
2019-02-19 09:07:18 +0800

    fix plot

--- pandas-demos/movie_data_analysis_test/slice_out_columns.py
+++ pandas-demos/movie_data_analysis_test/slice_out_columns.py
@@ -1,8 +1,7 @@
-
 # encode utf-8
 
 import unittest
-
+import matplotlib.pyplot as plt
 from movie_data_analysis.data_clean import *
 
 
@@ -12,14 +11,15 @@ class SliceOutColumns(unittest.TestCase):
         print(tags['tag'].head())
 
     def test_more_columns(self):
-        print(movies[['title','genres']].head())
+        print(movies[['title', 'genres']].head())
 
-    def test_(self):
+    def test_x_row(self):
         print(ratings[-10:])
 
     def test_value_counts(self):
         tag_counts = tags['tag'].value_counts()
         print(tag_counts[-10:])
         tag_counts[:10].plot(kind='bar', figsize=(15, 10))
+        plt.show()
 
     # def test_plot(self):
