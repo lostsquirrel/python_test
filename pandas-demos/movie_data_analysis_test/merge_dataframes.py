9358c2c192e437bcb018871dfd19c22aa033ff36
lostsquirrel <songli1988@gmail.com>
2019-02-20 13:58:42 +0800

    Combine aggreagation, merging, and filters to get useful analytics

--- pandas-demos/movie_data_analysis_test/merge_dataframes.py
+++ pandas-demos/movie_data_analysis_test/merge_dataframes.py
@@ -14,3 +14,21 @@ class MergeDataFrames(unittest.TestCase):
         t = movies.merge(tags, on='movieId', how='inner')
         print(t.head())
         print(t.columns)
+
+    def test_conbine(self):
+        avg_ratings = ratings.groupby('movieId', as_index=False).mean()
+        del avg_ratings['userId']
+        print(avg_ratings.head())
+        print()
+        box_office = movies.merge(avg_ratings, on='movieId', how='inner')
+        print(box_office.tail())
+        print()
+        is_highly_rated = box_office['rating'] >= 4.0
+
+        print(box_office[is_highly_rated][-5:])
+        print()
+        is_comedy = box_office['genres'].str.contains('Comedy')
+
+        print(box_office[is_comedy][:5])
+        print()
+        print(box_office[is_comedy & is_highly_rated][-5:])
