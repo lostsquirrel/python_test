bafa561ae4608b579f7205cad518c275bfcec1ab
lostsquirrel <songli1988@gmail.com>
2019-07-30 16:47:53 +0800

    add simple tests

---
+++ winreg-demos/simple.py
@@ -0,0 +1,13 @@
+import unittest
+import winreg
+
+class MyTestCase(unittest.TestCase):
+
+    def test_connect(self):
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
